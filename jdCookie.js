95007cdbb3597ae763c7bd245df78ebb32328c6d
DaiTtao <821772734@qq.com>
2021-11-15 13:48:30 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -3,7 +3,7 @@
  */
 //此处填写京东账号cookie。
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJg7-CjADD8RO0p98qrxtWKq5w_pIEQGtUeNxgF8DiIqzRQ_Sd1pVQY7-BiBTXdXfWmP_aLzhs;pt_pin=wdrWjefmTuukLj',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断环境变量里面是否有京东ck
